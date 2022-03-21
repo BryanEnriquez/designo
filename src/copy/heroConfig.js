6dd12e29adb699fa07c78633feac6fd2b01270be
A27x <bryan.a.enriquez@gmail.com>
2022-03-21 00:37:53 -0400

    Update cta to be more responsive

--- src/copy/heroConfig.js
+++ src/copy/heroConfig.js
@@ -10,27 +10,27 @@ export const heroConfig = {
     mods: { hero: MAIN, bg: MAIN, copy: MAIN },
   },
   '/web-design': {
-    heading: 'Web design',
+    heading: 'Web Design',
     p: 'We build websites that serve as powerful marketing tools and bring memorable brand experiences.',
     mods: { hero: DESIGN, bg: 'web', copy: DESIGN },
   },
   '/app-design': {
-    heading: 'App design',
+    heading: 'App Design',
     p: 'Our mobile designs bring intuitive digital solutions to your customers right at their fingertips.',
     mods: { hero: DESIGN, bg: 'app', copy: DESIGN },
   },
   '/graphic-design': {
-    heading: 'Graphic design',
+    heading: 'Graphic Design',
     p: 'We deliver eye-catching branding materials that are tailored to meet your business objectives.',
     mods: { hero: DESIGN, bg: 'graphic', copy: DESIGN },
   },
   '/about': {
-    heading: 'About us',
+    heading: 'About Us',
     p: "Founded in 2010, we are a creative agency that produces lasting results for our clients. We’ve partnered with many startups, corporations, and nonprofits alike to craft designs that make real impact. We’re always looking forward to creating brands, products, and digital experiences that connect with our clients' audiences.",
     mods: { hero: ABOUT, bg: ABOUT, copy: ABOUT },
   },
   '/contact': {
-    heading: 'Contact us',
+    heading: 'Contact Us',
     p: 'Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.',
     mods: { hero: CONTACT, bg: CONTACT, copy: CONTACT },
   },
